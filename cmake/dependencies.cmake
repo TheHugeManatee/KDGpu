b84f0c3ae7eba0e350c435130c4a954cc59f169a
Jakob Weiss <jakob.jw.weiss@gmail.com>
2025-12-20 21:38:44 +0100

    Revert "Use vcpkg to fetch dependencies"
    This reverts commit ecaf890cf63f1b1027f555a972d233f14cc30ee3.
    
    I'm not ready to abandon conan in favor of vcpkg just yet.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -6,30 +6,38 @@
 #
 # Contact KDAB at <info@kdab.com> for commercial licensing options.
 #
+include(FetchContent)
+
+# Note: FetchContent_MakeAvailable builds the project
+# if it contains a CMakeLists.txt, otherwise it does nothing.
+# ${package_SOURCE_DIR} ${package_BINARY_DIR} are made available by
+# MakeAvailable or Populate
+message(STATUS "Checking/updating dependencies. This may take a little while...
+    Set the FETCHCONTENT_QUIET option to OFF to get verbose output.
+"
+)
 
 # KDUtils
 if(KDGPU_BUILD_TESTS OR KDGPU_BUILD_KDGPUKDGUI)
-    find_package(KDUtils REQUIRED)
-    find_package(KDFoundation REQUIRED)
-    find_package(KDGui REQUIRED)
+    include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/dependencies/kdutils.cmake)
 endif()
 
 # spdlog
-find_package(spdlog REQUIRED)
+include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/dependencies/spdlog.cmake)
 
 # Vulkan & VMA
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/dependencies/vulkan.cmake)
-find_package(VulkanMemoryAllocator REQUIRED)
+include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/dependencies/vulkan_memory_allocator.cmake)
 
 if(KDGPU_BUILD_EXAMPLES)
     # glm
-    find_package(glm CONFIG REQUIRED)
+    include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/dependencies/glm.cmake)
 
     # stb (for stb image)
     include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/dependencies/stb.cmake)
 
     # imgui (for 2D UI)
-    find_package(imgui REQUIRED)
+    include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/dependencies/imgui.cmake)
 
     if(KDGPU_HLSL_SUPPORT)
         # dxc (for shader compilation)
@@ -37,9 +45,9 @@ if(KDGPU_BUILD_EXAMPLES)
     endif()
 endif()
 
-if(KDGPU_BUILD_KDXR)
+if(KDGPU_OPENXR_SUPPORT)
     # OpenXR
-    find_package(OpenXR REQUIRED)
+    include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/dependencies/openxr.cmake)
 endif()
 
 if(KDGPU_BUILD_TESTS)
